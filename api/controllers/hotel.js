b7eaa2a3b8cc891b4bfff0c00143e606fe381661
hritik01478 <hritikkumar01478@gmail.com>
2022-07-06 09:33:13 +0530

    home page connected with backend

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -45,9 +45,47 @@ export const getHotel = async (req, res, next) => {
 };
 
 export const getHotels = async (req, res, next) => {
+    const { min, max, ...others } = req.query
     try {
-        const hotels = await Hotel.find();
+        const hotels = await Hotel.find({
+            ...others, cheapestPrice: { $gt: min || 1, $lt: max || 999 }
+        }).limit(req.query.limit);
         res.status(200).json(hotels);
+    }
+    catch (err) {
+        next(err);
+    }
+};
+
+export const countByCity = async (req, res, next) => {
+    const cities = req.query.cities.split(",")
+
+    try {
+        const list = await Promise.all(cities.map(city => {
+            return Hotel.countDocuments({ city: city })
+        }))
+        res.status(200).json(list);
+    }
+    catch (err) {
+        next(err);
+    }
+};
+
+export const countByType = async (req, res, next) => {
+    try {
+        const hotelCount = await Hotel.countDocuments({ type: "hotel" })
+        const apartmentCount = await Hotel.countDocuments({ type: "apartment" })
+        const villaCount = await Hotel.countDocuments({ type: "villa" })
+        const resortCount = await Hotel.countDocuments({ type: "resort" })
+        const cabinCount = await Hotel.countDocuments({ type: "cabin" })
+
+        res.status(200).json([
+            { type: "hotel", count: hotelCount },
+            { type: "apartment", count: apartmentCount },
+            { type: "villa", count: villaCount },
+            { type: "resort", count: resortCount },
+            { type: "cabin", count: cabinCount }
+        ]);
     }
     catch (err) {
         next(err);
